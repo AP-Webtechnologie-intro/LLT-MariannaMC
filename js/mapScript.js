f99a5bdd498907eb335f6347fda33658198495ab
MariannaMC <145553733+MariannaMC@users.noreply.github.com>
2023-12-14 15:47:55 +0100

    javascript mapScript
    leaflet aan het aanpassen, maar het werkt nog niet.

--- js/mapScript.js
+++ js/mapScript.js
@@ -6,14 +6,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let CMRIcon = L.icon({
-  iconUrl: 'opdracht1/Logo/logo_cmr_christ_mon_redempteur-removebg-preview.png',
-  shadowUrl: 'images/coffee-icon-shadow.png',
+  iconUrl: 'Opdracht1/Kerk/Logo/logo_cmr_christ_mon_redemteur-removebg-preview.png',
 
   iconSize:     [60, 60], 
-  shadowSize:   [60, 60], 
   iconAnchor:   [0, 60], 
-  shadowAnchor: [0, 60],  
   popupAnchor:  [0, 0] 
 });
 
-L.marker([51.23009, 4.41616], {icon: coffeeIcon}).addTo(map);
+L.marker([51.23009, 4.41616], {icon: CMRIcon}).addTo(map);
